5daca2f186b269a91f17500b09b2f7c1494a09d1
xdswwwj <xdswwwj@knou.ac.kr>
2024-12-23 01:14:29 +0900

    fix : 필요없는 파일 삭제 및 파일 이름 변경

--- src/auth/dto/createUser.dto.ts
+++ src/auth/dto/createUser.dto.ts
@@ -3,25 +3,26 @@ import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({ description: '로그인 ID', example: 'xdswwwj' })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: '로그인 ID는 필수입니다.' })
   @IsString()
   userId: string;
 
   @ApiProperty({ description: '비밀번호', example: 'skdicls1' })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: '비밀번호는 필수입니다.' })
   @IsString()
   password: string;
 
-  @ApiProperty({ description: '비밀번호', example: 'skdicls1' })
-  @IsNotEmpty()
+  @ApiProperty({ description: '비밀번호 확인', example: 'skdicls1' })
+  @IsNotEmpty({ message: '비밀번호 확인은 필수입니다.' })
   @IsString()
   passwordConfirm: string;
 
-  @ApiProperty({
-    description: '사용자 이름',
-    example: '김도현',
-    required: false,
-  })
+  @ApiProperty({ description: '사용자 휴대폰 번호', example: '01021678895' })
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @ApiProperty({ description: '사용자 이름', example: '김도현' })
   @IsOptional()
   @IsString()
   name?: string;
@@ -36,8 +37,7 @@ export class CreateUserDto {
 
   @ApiProperty({
     description: '프로필 이미지 URL',
-    example:
-      'http://k.kakaocdn.net/dn/me51W/btsK5Dkhb7e/sWSrkiHre6Bu1bQSoQuHtk/img_640x640.jpg',
+    example: 'http://k.kakaocdn.net/dn/me51W/btsK5Dkhb7e/sWSrkiHre6Bu1bQSoQuHtk/img_640x640.jpg',
     required: false,
   })
   @IsOptional()
