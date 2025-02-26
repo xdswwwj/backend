23632a346b35a9e67a22290a336f7a610f29eac2
xdswwwj <xdswwwj@knou.ac.kr>
2025-02-27 03:32:22 +0900

    진짜 마지막 테스트

--- src/config/global.config.ts
+++ src/config/global.config.ts
@@ -1,4 +1,4 @@
-export const FRONT_BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5173' : 'https://sanirang.kr';
+export const FRONT_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://sanirang.kr' : 'http://localhost:5173';
 export const BACK_BASE_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://api.sanirang.kr';
+  process.env.NODE_ENV === 'production' ? 'https://api.sanirang.kr' : 'http://localhost:3000';
 export const KAKAO_CALLBACK_URL = BACK_BASE_URL + '/auth/kakao/redirect';
