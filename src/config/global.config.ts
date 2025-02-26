17cb5ba30fd0a0bbb9841f7e0e6e40e4569c1351
xdswwwj <xdswwwj@knou.ac.kr>
2025-02-27 02:54:53 +0900

    url 설정

--- src/config/global.config.ts
+++ src/config/global.config.ts
@@ -1,4 +1,3 @@
-export const FRONT_BASE_URL =
-  process.env.NODE_ENV === 'development' ? process.env.FRONT_DEV_BASE_URL : process.env.FRONT_PROD_BASE_URL;
+export const FRONT_BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5173' : 'https://sanirang.kr';
 export const BACK_BASE_URL =
-  process.env.NODE_ENV === 'development' ? process.env.BACK_DEV_BASE_URL : process.env.BACK_PROD_BASE_URL;
+  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://api.sanirang.kr';
