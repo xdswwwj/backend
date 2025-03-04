cd492f481e8b2ce73b01192a308b3423de37651b
xdswwwj <xdswwwj@knou.ac.kr>
2025-03-05 07:44:23 +0900

    jwt expiresIn 변경

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
     };
     return this.jwtService.sign(payload, {
       secret: process.env.JWT_SECRET,
-      expiresIn: '1d',
+      expiresIn: '24h',
     });
   }
 
